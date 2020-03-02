65811f7f56f986d59b7dbb07ebed6a03b74f2519
Bruno Randolf <br1@einfach.org>
2020-03-02 10:52:42 +0000

    Add emtpy files for BLE

---
+++ dfu_ble.c
@@ -0,0 +1,9 @@
+#include "dfu_ble.h"
+
+bool ble_write(uint8_t* req, size_t len)
+{
+}
+
+const uint8_t* ble_read(void)
+{
+}
---
+++ dfu_ble.h
@@ -0,0 +1,30 @@
+/*
+ * nrfdfu - Nordic DFU Upgrade Utility
+ *
+ * Copyright (C) 2019 Bruno Randolf (br1@einfach.org)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef DFU_BLE_H
+#define DFU_BLE_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+bool ble_write(uint8_t* req, size_t len);
+const uint8_t* ble_read(void);
+
+#endif
